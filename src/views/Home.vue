cb7e0717122367cc902da4895028efc2b9b3c168
Kenta Iwasaki <kenta@perlin.net>
2019-01-19 13:10:31 +0800

    all: initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <article class="pa3 pa5-ns">
+        <h1 class="f3 f1-m f-headline-l">Title</h1>
+        <p class="measure lh-copy">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+        <p class="measure lh-copy">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+    </article>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import "tachyons/css/tachyons.css";
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        data() {
+            return {
+                someText: "bla",
+            }
+        }
+    }
 </script>
+
+<style>
+    .lh-copy {
+        line-height: 1.5;
+    }
+
+    .pa3 {
+        padding: 1rem;
+    }
+
+    .f3 {
+        font-size: 1.5rem;
+    }
+
+    .measure {
+        max-width: 30em;
+    }
+
+    @media screen and (min-width: 30em) {
+        .pa5-ns {
+            padding: 4rem;
+        }
+    }
+
+    @media screen and (min-width: 30em) and (max-width: 60em) {
+        .f1-m {
+            font-size: 3rem;
+        }
+    }
+
+    @media screen and (min-width: 60em) {
+        .f-headline-l {
+            font-size: 6rem;
+        }
+    }
+</style>
